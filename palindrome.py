30842bc5190fbedb342743aeaab995abddd824ac
OscarABazanez <oscar-bazanez@hotmail.com>
2021-07-31 16:14:49 -0500

    Usando tipado estatico con palindromo, numero primo y agregando gitignore

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+
+def is_palindrome(string: str) -> bool:
+    string = string.replace(" ","").lower()
+    return string == string[::-1]
+
+def is_prime(number: int) -> bool:
+    result_list = [x for x in range(2,number) if number % x ==0]
+    return len(result_list) == 0
+
+def run():
+    print('Es un palindromo:',is_palindrome(321)) #True
+    print('2 Es un numero primo :',is_prime(2)) # True
+    print('14 Es un numero primo :',is_prime(14)) # False
+
+if __name__ == '__main__':
+    run()
